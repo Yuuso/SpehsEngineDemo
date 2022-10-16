7396e46ea6ffc315291f5dc3c547da8ecbc8fddd
Juuso Turunen <juusoturunen94@hotmail.com>
2022-10-16 22:27:14 +0300

    Fix post build

--- SpehsEngineDemo/Demo/post_build.bat
+++
@@ -1,7 +0,0 @@
-ECHO Demo post build script start
-ECHO Copy DLLs to TargetDir
-
-DEL %TargetDir%*.dll
-COPY %SPEHSENGINE_ROOT%Dependencies\dll\x64\SDL2.dll %TargetDir%SDL2.dll /y
-
-ECHO Demo post build script end
